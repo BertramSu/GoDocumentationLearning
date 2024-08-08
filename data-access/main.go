98bcf3abbda332a2bb769530677125d81e0eaceb
BertramSu <40878105+BertramSu@users.noreply.github.com>
2024-08-08 00:07:01 -0500

    Add R and C for albums
    Read and create albums

---
+++ data-access/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type Album struct {
+	ID     int64   `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float32 `json:"price"`
+}
+
+type Config struct {
+	Username     string
+	Password     string
+	Address      string
+	DatabaseName string
+}
+
+var cfg = Config{
+	Username:     "admin",
+	Password:     "passwordHere",
+	Address:      "localhost:5432",
+	DatabaseName: "album_db",
+}
+
+var connStr = fmt.Sprintf("postgres://%s:%s@%s/%s", cfg.Username, cfg.Password, cfg.Address, cfg.DatabaseName)
+
+func main() {
+	albums, err := getAllAlbums()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v", albums)
+
+	album, err := getAlbumById(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v", album)
+
+	/*
+		newAlbum := Album{
+			Title:  "Pretending",
+			Artist: "Fletcher",
+			Price:  0.99,
+		}
+		insertInt, err := insertAlbum(newAlbum)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Album inserted with id: %v", insertInt)
+
+		albumss, err := getAllAlbums()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Albums found: %v", albumss)
+	*/
+}
+
+func getAllAlbums() ([]Album, error) {
+	// Connect to the database
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Query example
+	query := "SELECT id, title, artist, price FROM album"
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		log.Fatalf("Query failed getAllAlbums: %v", err)
+	}
+	defer rows.Close()
+
+	return pgx.CollectRows(rows, pgx.RowToStructByName[Album])
+}
+
+func getAlbumById(id int) (Album, error) {
+	// Connect to the database
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	query := "SELECT id, title, artist, price FROM album WHERE id = @id"
+
+	args := pgx.NamedArgs{
+		"id": id,
+	}
+	// Query example
+	rows, err := conn.Query(context.Background(), query, args)
+	if err != nil {
+		log.Fatalf("Query failed getAlbumById): %v", err)
+	}
+	defer rows.Close()
+
+	return pgx.CollectOneRow(rows, pgx.RowToStructByName[Album])
+}
+
+func insertAlbum(album Album) (int, error) {
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+		return 0, err
+	}
+	defer conn.Close(context.Background())
+
+	query := "INSERT INTO album (title, artist, price) VALUES (@title, @artist, @price) RETURNING id"
+
+	args := pgx.NamedArgs{
+		"title":  album.Title,
+		"artist": album.Artist,
+		"price":  album.Price,
+	}
+
+	var id int
+	qErr := conn.QueryRow(context.Background(), query, args).Scan(&id)
+	if qErr != nil {
+		log.Fatalf("Query failed insertAlbum): %v", err)
+		return 0, qErr
+	}
+
+	return id, qErr
+}
